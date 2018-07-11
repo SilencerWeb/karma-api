aa8fa9d46560b393dee4943c15076b560aeac3f9
SilencerWeb <silencer.web@gmail.com>
2018-07-11 11:47:01 +0500

    Add ability to update and delete person's avatar

--- src/resolvers/mutations/person.js
+++ src/resolvers/mutations/person.js
@@ -30,16 +30,47 @@ const createPerson = (_, args, context, info) => {
 const updatePerson = async(_, args, context, info) => {
   const userId = getUserId(context);
 
+  const data = {
+    name: args.name,
+    position: args.position,
+    description: args.description,
+  };
+
+  if (args.deleteAvatar) {
+    const person = await context.prisma.query.person(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `
+        {
+          avatar {
+            id
+          }
+        }
+      `,
+    );
+
+    if (person.avatar && person.avatar.id) {
+      data.avatar = {
+        disconnect: {
+          id: person.avatar.id,
+        },
+      };
+    }
+  } else if (args.avatar) {
+    data.avatar = {
+      connect: { id: args.avatar },
+    };
+  }
+
   return context.prisma.mutation.updatePerson(
     {
       where: {
         id: args.id,
       },
-      data: {
-        name: args.name,
-        position: args.position,
-        description: args.description,
-      },
+      data: data,
     },
     info,
   );
